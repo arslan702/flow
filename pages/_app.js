1ea1d609072ad5a424b52b6f00354557506aa4c4
mycomputer <muhammadarslanjabbar@gmail.com>
2024-02-23 14:32:24 +0500

    second-commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,12 @@
-
-import Banner from "@/Components/Banner/Banner";
-import Faq2 from "@/Components/Faq2";
-import Faqs from "@/Components/Faqs";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header/Header";
-import Help from "@/Components/Help";
-import People from "@/Components/People";
-import Pricing from "@/Components/Pricing";
-import Review from "@/Components/Review";
-import Why from "@/Components/Why";
-import Work from "@/Components/Work";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <Banner/>
       <Component {...pageProps} />
-      
-      <Help/>
-      <Review/>
-      <Work/>
-      <Why/>
-      <People/>
-      <Pricing/>
-      <Faqs/>
-      <Faq2/>
       <Footer/>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Banner from "@/Components/Banner/Banner";
+import Help from "@/Components/Help";
+import Review from "@/Components/Review";
+import Work from "@/Components/Work";
+import Why from "@/Components/Why";
+import People from "@/Components/People";
+import Pricing from "@/Components/Pricing";
+import Faqs from "@/Components/Faqs";
+import Faq2 from "@/Components/Faq2";
 
 export default function Home() {
   return (
     <main
       className=''
     >
-      
+      <Banner/>
+      <Help/>
+      <Review/>
+      <Work/>
+      <Why/>
+      <People/>
+      <Pricing/>
+      <Faqs/>
+      <Faq2/>
     </main>
   );
 }
